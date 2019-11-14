69c12ea7e28624b5460b02dfc73bfd0371ed6537
Andrew Serong <andrewserong@users.noreply.github.com>
2019-11-14 18:26:33 +1100

    Revert changes to customer home routing and add root section to handle redirects

--- client/sections.js
+++ client/sections.js
@@ -459,7 +459,7 @@ const sections = [
 	},
 	{
 		name: 'home',
-		paths: [ '/', '/home' ],
+		paths: [ '/home' ],
 		module: 'my-sites/customer-home',
 		secondary: true,
 		group: 'sites',
@@ -470,6 +470,12 @@ const sections = [
 		module: 'my-sites/hosting',
 		secondary: true,
 		group: 'sites',
+	},
+	{
+		name: 'root',
+		paths: [ '/' ],
+		module: 'root',
+		secondary: false,
 	},
 ];
 
