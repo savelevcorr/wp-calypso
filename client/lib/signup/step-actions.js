de30ce82d286df1b0ad2a6717f0e8a5f6e1a2c0b
Philip Jackson <p-jackson@live.com>
2019-11-19 20:46:48 +1300

    Tell backend when user recaptcha fails or doesn't load in time

--- client/lib/signup/step-actions.js
+++ client/lib/signup/step-actions.js
@@ -366,7 +366,18 @@ export function launchSiteApi( callback, dependencies ) {
 export function createAccount(
 	callback,
 	dependencies,
-	{ userData, flowName, queryArgs, service, access_token, id_token, oauth2Signup, recaptchaToken },
+	{
+		userData,
+		flowName,
+		queryArgs,
+		service,
+		access_token,
+		id_token,
+		oauth2Signup,
+		recaptchaDidntLoad,
+		recaptchaFailed,
+		recaptchaToken,
+	},
 	reduxStore
 ) {
 	const state = reduxStore.getState();
@@ -434,6 +445,8 @@ export function createAccount(
 							oauth2_redirect: queryArgs.oauth2_redirect && '0@' + queryArgs.oauth2_redirect,
 					  }
 					: null,
+				recaptchaDidntLoad ? { 'g-recaptcha-didnt-load': true } : null,
+				recaptchaFailed ? { 'g-recaptcha-failed': true } : null,
 				recaptchaToken ? { 'g-recaptcha-response': recaptchaToken } : null
 			),
 			( error, response ) => {
