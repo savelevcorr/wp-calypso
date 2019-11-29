68dec037cf7eb7e0d1a4f2585479b6fc82f66bb6
Jon Surrell <jon.surrell@automattic.com>
2019-11-29 11:08:09 +0100

    Implement some working routing

--- client/landing/gutenboarding/gutenboard.tsx
+++ client/landing/gutenboarding/gutenboard.tsx
@@ -15,12 +15,13 @@ import {
 	DropZoneProvider,
 	KeyboardShortcuts,
 } from '@wordpress/components';
-import { createBlock, registerBlockType } from '@wordpress/blocks';
+import { createBlock, registerBlockType, BlockInstance } from '@wordpress/blocks';
 import { rawShortcut, displayShortcut, shortcutAriaLabel } from '@wordpress/keycodes';
 import '@wordpress/format-library';
 import classnames from 'classnames';
 import React, { useState } from 'react';
 import '@wordpress/components/build-style/style.css';
+import * as RR from 'react-router-dom';
 
 /**
  * Internal dependencies
@@ -43,10 +44,74 @@ const toggleSidebarShortcut = {
 };
 
 registerBlockType( name, settings );
+registerBlockType( 'a8c/redir', {
+	title: 'boom',
+	category: 'layout',
+	attributes: { to: { type: 'string' } },
+	edit: ( { attributes } ) => {
+		return <RR.Redirect to={ attributes.to } />;
+	},
+} );
+
+registerBlockType( 'a8c/under-construction', {
+	title: 'boom2',
+	category: 'layout',
+	attributes: {},
+	edit: () => {
+		return (
+			<>
+				<RR.Link to="/">Back</RR.Link>
+				<svg width="320pt" height="320pt" viewBox="0 0 320 320">
+					<path
+						fill="#ffff02"
+						stroke="#ffff02"
+						d=" M 130.9 25.8 C 139.7 17.1 152.6 11.9 165.0 14.1 C 174.7 14.8 183.5 20.1 190.2 26.8 C 224.5 61.2 258.9 95.5 293.2 129.9 C 296.5 134.3 300.4 138.6 302.1 144.0 C 305.2 150.8 305.1 158.6 304.2 165.9 C 303.3 174.4 297.9 181.3 292.4 187.4 C 256.7 223.0 221.0 258.7 185.4 294.4 C 182.8 296.7 179.6 297.9 176.6 299.5 C 171.6 302.5 165.5 302.3 160.0 303.6 C 155.1 302.5 150.1 302.3 145.4 300.5 C 141.5 298.5 137.3 297.0 134.1 293.9 C 99.6 259.3 65.1 224.9 30.6 190.4 C 27.1 187.2 24.3 183.3 21.2 179.6 C 15.0 171.0 14.4 159.9 15.9 149.8 C 17.7 142.0 22.2 135.3 27.3 129.3 C 61.9 94.8 96.3 60.3 130.9 25.8 Z"
+						style={ { stroke: 'black', strokeWidth: 12, strokeLinecap: 'round' } }
+					/>
+					<circle cx={ 168 } cy={ 100 } r={ 14 } fill="#000" />
+
+					<path
+						fill="#010102"
+						stroke="#010102"
+						d=" M 101.1 117.8 C 102.1 114.3 103.3 110.5 106.7 108.6 C 117.2 108.7 127.6 108.7 138.0 108.7 C 140.1 108.6 142.1 109.1 144.1 109.5 C 148.2 116.1 153.2 122.1 157.5 128.6 C 159.7 131.9 161.8 136.1 159.8 140.0 C 154.0 150.7 148.0 161.3 142.3 172.1 C 155.4 181.7 167.5 192.5 180.6 202.0 C 184.4 197.9 189.2 193.4 195.2 194.8 C 197.4 195.6 200.0 195.9 201.6 197.7 C 203.7 200.0 205.5 202.6 206.9 205.4 C 209.0 204.4 211.7 204.0 213.8 205.2 C 217.4 207.5 219.2 211.9 219.3 216.0 C 221.5 217.0 223.3 218.4 224.5 220.4 C 230.1 220.0 234.4 224.1 236.6 228.8 C 211.7 229.0 186.9 228.7 162.0 229.1 C 155.9 228.5 149.7 229.6 143.5 228.7 C 144.8 227.2 145.9 225.5 147.0 223.8 C 149.9 222.5 152.9 221.1 156.2 221.1 C 157.9 220.7 159.6 220.1 158.8 217.9 C 160.5 215.3 162.7 212.4 166.3 213.1 C 168.2 209.3 171.3 206.3 175.0 204.2 C 163.8 195.0 152.5 186.0 141.3 176.8 C 140.8 175.5 139.1 176.4 139.5 177.4 C 142.0 180.1 144.9 183.1 144.7 187.0 C 144.9 189.5 143.1 191.6 142.2 193.8 C 139.3 200.7 135.1 207.1 131.8 213.8 C 129.8 218.0 127.6 222.2 125.2 226.2 C 122.6 228.2 118.7 229.6 115.5 228.1 C 113.5 226.6 112.3 224.3 111.6 222.0 C 113.6 214.7 117.9 208.5 120.7 201.5 C 122.9 196.7 125.4 192.0 127.7 187.3 C 121.8 180.0 116.0 172.6 109.8 165.5 C 109.7 172.9 110.1 180.4 109.7 187.8 C 106.8 199.4 103.6 211.0 100.6 222.7 C 99.7 227.6 92.9 230.3 88.9 227.0 C 85.3 225.1 86.1 220.6 86.3 217.2 C 88.6 207.8 91.0 198.5 93.3 189.1 C 93.7 177.9 93.1 166.7 93.6 155.5 C 93.6 151.0 98.2 149.3 100.4 146.1 C 98.0 145.3 96.3 143.3 96.4 140.8 C 94.1 139.2 92.0 137.3 89.5 136.0 C 88.5 133.9 89.9 132.2 91.8 131.5 C 93.5 133.1 95.4 134.5 97.3 135.7 C 98.6 129.8 99.7 123.8 101.1 117.8 Z"
+					/>
+					<path
+						fill="#ffff02"
+						stroke="#ffff02"
+						d=" M 113.0 123.3 C 116.6 123.4 120.1 123.4 123.6 123.4 C 119.3 128.0 114.4 132.1 110.3 136.9 C 110.8 132.3 112.1 127.9 113.0 123.3 Z"
+					/>
+					<path
+						fill="#ffff02"
+						stroke="#ffff02"
+						d=" M 122.7 156.0 C 128.2 153.5 133.7 151.2 139.2 148.7 C 137.2 153.8 133.8 158.0 131.8 163.0 C 128.6 160.8 125.5 158.6 122.7 156.0 Z"
+					/>
+				</svg>
+			</>
+		);
+	},
+} );
 
 const onboardingBlock = createBlock( name, {} );
+const redirect = createBlock( 'a8c/redir', { to: '/' } );
+const uc = createBlock( 'a8c/under-construction', {} );
 
 export function Gutenboard() {
+	const r = RR.useRouteMatch( '*' );
+
+	let content: BlockInstance;
+	switch ( r?.url ) {
+		case '/':
+			content = onboardingBlock;
+			break;
+
+		case '/fse':
+			content = uc;
+			break;
+
+		default:
+			content = redirect;
+	}
+
 	const [ isEditorSidebarOpened, updateIsEditorSidebarOpened ] = useState( false );
 
 	const toggleGeneralSidebar = () => updateIsEditorSidebarOpened( isOpen => ! isOpen );
@@ -72,7 +137,7 @@ export function Gutenboard() {
 							toggleGeneralSidebar={ toggleGeneralSidebar }
 							toggleSidebarShortcut={ toggleSidebarShortcut }
 						/>
-						<BlockEditorProvider value={ [ onboardingBlock ] } settings={ { templateLock: 'all' } }>
+						<BlockEditorProvider value={ [ content ] } settings={ { templateLock: 'all' } }>
 							<div className="edit-post-layout__content">
 								<div
 									className="edit-post-visual-editor editor-styles-wrapper"
--- client/landing/gutenboarding/onboarding-block/edit.tsx
+++ client/landing/gutenboarding/onboarding-block/edit.tsx
@@ -3,8 +3,9 @@
  */
 import { __ as NO__ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { Button } from '@wordpress/components';
+import { Link } from 'react-router-dom';
 
 /**
  * Internal dependencies
@@ -15,6 +16,43 @@ import StepperWizard from './stepper-wizard';
 import VerticalSelect from './vertical-select';
 import SiteTitle from './site-title';
 import './style.scss';
+
+interface LinkButtonProps extends Button.AnchorProps {
+	navigate: () => {};
+}
+// Render `Link` with `@wordpress/components` `Button`
+// Needs to handle `navigate` onClick
+// from https://github.com/ReactTraining/react-router/blob/e81dfa2d01937969ee3f9b1f33c9ddd319f9e091/packages/react-router-dom/modules/Link.js#L21
+const LinkButton = forwardRef< HTMLAnchorElement, LinkButtonProps >(
+	( { navigate, onClick, ...rest }, forwardedRef ) => {
+		const { target } = rest;
+		const props = {
+			...rest,
+			onClick: ( event: React.MouseEvent< HTMLAnchorElement, MouseEvent > ) => {
+				try {
+					if ( onClick ) onClick( event );
+				} catch ( ex ) {
+					event.preventDefault();
+					throw ex;
+				}
+
+				if (
+					! event.defaultPrevented && // onClick prevented default
+					event.button === 0 && // ignore everything but left clicks
+					( ! target || target === '_self' ) && // let browser handle "target=_blank" etc.
+					! isModifiedEvent( event ) // ignore clicks with modifier keys
+				) {
+					event.preventDefault();
+					navigate();
+				}
+			},
+		};
+		return <Button ref={ forwardedRef } { ...props } />;
+	}
+);
+function isModifiedEvent( event ) {
+	return !! ( event.metaKey || event.altKey || event.ctrlKey || event.shiftKey );
+}
 
 export default function OnboardingEdit() {
 	const { siteVertical, siteTitle } = useSelect( select => select( STORE_KEY ).getState() );
@@ -33,9 +71,14 @@ export default function OnboardingEdit() {
 				</StepperWizard>
 				{ isFilledFormValue( siteVertical ) && (
 					<div className="onboarding-block__footer">
-						<Button className="onboarding-block__question-skip" isLink>
+						<Link
+							component={ LinkButton }
+							to="/fse"
+							isLink
+							className="onboarding-block__question-skip"
+						>
 							{ NO__( "Don't know yet" ) } →
-						</Button>
+						</Link>
 					</div>
 				) }
 			</div>
