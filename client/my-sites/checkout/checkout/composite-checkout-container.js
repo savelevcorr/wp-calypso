7a74488266dbd8772d87c845b40f9e3a46b6ef7b
Payton Swick <payton@foolord.com>
2019-12-03 11:21:42 -0500

    Pass contact info to createPayPalMethod

--- client/my-sites/checkout/checkout/composite-checkout-container.js
+++ client/my-sites/checkout/checkout/composite-checkout-container.js
@@ -122,6 +122,11 @@ const stripeMethod = createStripeMethod( {
 
 const paypalMethod = createPayPalMethod( {
 	getSiteId: () => select( 'wpcom' )?.getSiteId?.(),
+	getCountry: () => select( 'wpcom' )?.getContactInfo?.()?.country?.value,
+	getPostalCode: () => select( 'wpcom' )?.getContactInfo?.()?.postalCode?.value,
+	getPhoneNumber: () => select( 'wpcom' )?.getContactInfo?.()?.phoneNumber?.value,
+	getSubdivisionCode: () => select( 'wpcom' )?.getContactInfo?.()?.state?.value,
+	getDomainDetails,
 	registerStore: registerStore,
 	makePayPalExpressRequest: mockPayPalExpressRequest,
 } );
