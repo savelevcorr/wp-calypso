ee2fd9d55921701434549733fcded7e28044d795
mmtr <miguel.torres@automattic.com>
2019-12-06 16:23:26 +0100

    WP.com block editor: Exclude features from Jetpack sites

--- apps/wpcom-block-editor/src/common/index.js
+++ apps/wpcom-block-editor/src/common/index.js
@@ -1,10 +1,12 @@
 /**
  * Internal dependencies
  */
-import './disable-nux-tour';
 import './reorder-block-categories';
 import './rich-text';
-import './style.scss';
 import './switch-to-classic';
 import './tracking';
-import './unregister-experimental-blocks';
+
+/**
+ * Style dependencies
+ */
+import './style.scss';
